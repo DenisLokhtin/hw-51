6f63b32e58f4385579655240b6acb00dc918faa3
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-07-15 18:07:47 +0600

    компонент готов

---
+++ react-app/src/numbers/numbers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Numbers = props => {
+    return (
+        <div className="number">
+            <p>{props.number}</p>
+        </div>
+    )
+}
+
+export default Numbers;
